d7a375665a19e02785f2e0c90b2c6721875fedb9
moutamid <lumianokia188@gmail.com>
2024-10-20 13:11:26 +0500

    commit

--- app/src/main/java/com/moutamid/fallsdeliveryadmin/SplashActivity.java
+++ app/src/main/java/com/moutamid/fallsdeliveryadmin/SplashActivity.java
@@ -17,7 +17,7 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
 
         new Handler().postDelayed(() -> {
-            startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            startActivity(new Intent(SplashActivity.this, LoginActivity.class));
             finish();
         }, 2000);
 
